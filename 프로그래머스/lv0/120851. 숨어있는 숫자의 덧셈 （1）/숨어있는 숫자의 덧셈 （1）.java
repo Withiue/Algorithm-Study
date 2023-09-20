3214e99339bf0c9ca6ec015714bc33cc021bc252
Yeonjae Choi <fox2yj@naver.com>
2023-09-20 20:18:22 +0900

    [level 0] Title: 숨어있는 숫자의 덧셈 (1), Time: 0.28 ms, Memory: 83.2 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120851. 숨어있는 숫자의 덧셈 （1）/숨어있는 숫자의 덧셈 （1）.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int solution(String my_string) {
+        int answer = 0;
+        my_string = my_string.replaceAll("[^0-9]", "");
+        String[] arr = my_string.split("");
+        for(int i = 0; i < my_string.length(); i++) {
+            answer += Integer.parseInt(arr[i]);
+        }
+        return answer;
+    }
+}
