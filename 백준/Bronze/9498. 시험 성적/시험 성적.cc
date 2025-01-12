b56b5aa6a86a37049900ebcb3252238989814834
Yeonjae Choi <fox2yj@naver.com>
2025-01-12 20:09:47 +0900

    [Bronze V] Title: 시험 성적, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/9498. 시험 성적/시험 성적.cc
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+int score;
+int main(void) {
+    ios::sync_with_stdio(0); cin.tie(0);
+    cin >> score;
+    if(score >= 90 && score <=100) cout << 'A';
+    else if(score >= 80 && score <=89) cout << 'B';
+    else if(score >= 70 && score <=79) cout << 'C';
+    else if(score >= 60 && score <=69) cout << 'D';
+    else cout << 'F';
+}
