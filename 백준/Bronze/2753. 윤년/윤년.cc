d917219be8ba38285c8febb7592d15bf0c526e38
Yeonjae Choi <fox2yj@naver.com>
2025-01-13 12:19:35 +0900

    [Bronze V] Title: 윤년, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2753. 윤년/윤년.cc
@@ -0,0 +1,8 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main(void) {
+    int year;
+    cin>>year;
+    if(((year%4==0) && (year%100!=0)) || (year%400==0)) cout<<'1';
+    else cout<<'0';
+}
