3dc5ed108634574b6868f0ebb899311b80f11eb2
Yeonjae Choi <fox2yj@naver.com>
2025-01-14 11:08:45 +0900

    [Bronze III] Title: 별 찍기 - 6, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2443. 별 찍기 － 6/별 찍기 － 6.cc
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+int N;
+int main(void) {
+    ios::sync_with_stdio(0); cin.tie(0);
+    cin>>N;
+    for(int i=1; i<=N; i++) {
+        for(int j=0; j<i-1; j++) cout<<" ";
+        for(int j=0; j<2*(N-i)+1; j++) cout<<'*';
+        cout<<'\n';
+    }
+}
