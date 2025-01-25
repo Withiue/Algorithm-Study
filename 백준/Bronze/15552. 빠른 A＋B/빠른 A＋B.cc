db2b8c8dc3de34ab7d8b1c780beef2d3da6d32b2
Yeonjae Choi <fox2yj@naver.com>
2025-01-25 15:48:32 +0900

    [Bronze IV] Title: 빠른 A+B, Time: 192 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/15552. 빠른 A＋B/빠른 A＋B.cc
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+int T, A, B;
+int main(void) {
+    ios::sync_with_stdio(0); cin.tie(0);
+    cin>>T;
+    for(int i=0; i<T; i++) {
+        cin>>A>>B;
+        cout<<A+B<<'\n';
+    }
+}
