cc498282b42b701d74937c4ccd03f598866799a0
Yeonjae Choi <yeonjaechoi25@gmail.com>
2025-10-13 23:17:31 +0900

    [Silver II] Title: 로또, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/6603. 로또/로또.py
+++ 백준/Silver/6603. 로또/로또.py
@@ -1,15 +1,15 @@
-def backtracking(start, depth):
-    """백트래킹을 이용한 조합 생성"""
+def backtracking(cnt, start):
+    #백트래킹을 이용한 조합 생성
     # 6개를 모두 선택했으면 출력
-    if depth == 6:
-        print(' '.join(map(str, result)))
+    if cnt == 6:
+        print(*result)
         return
     
     # start 인덱스부터 끝까지 탐색
     for i in range(start, len(s)):
         result.append(s[i])  # 현재 수를 선택
-        backtracking(i + 1, depth + 1)  # 다음 인덱스부터 탐색
-        result.pop()  # 백트래킹: 선택 취소
+        backtracking(cnt + 1, i + 1)  # 다음 인덱스부터 탐색
+        result.pop()  # 백트래킹
 
 while True:
     # 입력 받기
@@ -23,8 +23,5 @@ while True:
     s = data[1:]  # 집합 S의 원소들
     result = []  # 선택된 수들을 저장할 리스트
     
-    # 백트래킹 시작
     backtracking(0, 0)
-    
-    # 각 테스트 케이스 사이에 빈 줄 출력
     print()
