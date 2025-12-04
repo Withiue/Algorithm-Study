ec2cee6744aafb99f7f39d2677fba030610f7430
Yeonjae Choi <yeonjaechoi25@gmail.com>
2025-12-04 09:30:48 +0900

    [level 2] Title: 전화번호 목록, Time: 507.05 ms, Memory: 45.9 MB -BaekjoonHub

--- 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.py
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.py
@@ -1,6 +1,11 @@
 def solution(phone_book):
-    phone_book.sort()  # 정렬해서 인접한 전화번호만 탐색하기
-    for i in range(len(phone_book) - 1):
-        if phone_book[i+1].startswith(phone_book[i]):
-            return False
+    phone_dict = {}
+    for number in phone_book:
+        phone_dict[number] = 1
+
+    for number in phone_book:
+        for i in range(1, len(number)):
+            prefix = number[:i]
+            if prefix in phone_dict:
+                return False
     return True
