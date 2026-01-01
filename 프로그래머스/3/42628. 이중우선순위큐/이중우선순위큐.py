8a8af605677c822bdb4c4d1f75bf95daf24cb6ad
Yeonjae Choi <yeonjaechoi25@gmail.com>
2026-01-01 18:27:00 +0900

    [level 3] Title: 이중우선순위큐, Time: 544.09 ms, Memory: 15.5 MB -BaekjoonHub

---
+++ 프로그래머스/3/42628. 이중우선순위큐/이중우선순위큐.py
@@ -0,0 +1,30 @@
+import heapq
+
+def solution(operations):
+    heap = []
+    
+    for op in operations:
+        command, value = op.split()
+        num = int(value)
+        
+        if command == 'I':
+            # 데이터를 넣을 때는 최소 힙 형태 유지
+            heapq.heappush(heap, num)
+        
+        elif command == 'D':
+            if not heap:  # 큐가 비어있으면 무시
+                continue
+                
+            if num == 1:
+                # 최댓값 삭제
+                heap.remove(max(heap))
+                heapq.heapify(heap)  # 다시 힙으로 만들기
+            else:
+                # 최솟값 삭제
+                heapq.heappop(heap)
+                
+    if not heap:  # 큐가 비어있으면
+        return [0, 0]
+    else:  # [최대, 최소] 반환
+        return [max(heap), min(heap)]
+        
