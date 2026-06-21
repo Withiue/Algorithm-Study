312d6254b3d9e7c7ab6067d4f2341675c45a6409
Yeonjae Choi <yeonjaechoi25@gmail.com>
2026-06-21 16:02:54 +0900

    [level 0] Title: 등차수열의 특정한 항만 더하기, Time: 0.01 ms, Memory: 11.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/181931. 등차수열의 특정한 항만 더하기/등차수열의 특정한 항만 더하기.py
@@ -0,0 +1,10 @@
+def solution(a, d, included):
+    answer = 0
+    
+    n = len(included)
+    
+    for i in range(n):
+        if included[i]:
+            answer += a + (d * i)
+    
+    return answer
