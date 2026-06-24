556c8bbcaf70a0e9439bae117c82ab2e15bc8155
Yeonjae Choi <yeonjaechoi25@gmail.com>
2026-06-24 16:50:51 +0900

    [level 0] Title: 배열 만들기 4, Time: 18.47 ms, Memory: 15.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/181918. 배열 만들기 4/배열 만들기 4.py
@@ -0,0 +1,14 @@
+def solution(arr):
+    stk = []
+    i = 0  # 초기값 설정
+    while i < len(arr):
+        if stk:
+            if stk[-1] < arr[i]:
+                stk.append(arr[i])
+                i += 1
+            else:
+                stk = stk[:-1]
+        else:
+            stk.append(arr[i])
+            i += 1
+    return stk
