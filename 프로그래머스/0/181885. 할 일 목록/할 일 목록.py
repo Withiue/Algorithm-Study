7f00a69ff85311bd45bf8f58df3c84d99524335f
Yeonjae Choi <yeonjaechoi25@gmail.com>
2026-06-30 23:10:39 +0900

    [level 0] Title: 할 일 목록, Time: 0.03 ms, Memory: 11.3 MB -BaekjoonHub

---
+++ 프로그래머스/0/181885. 할 일 목록/할 일 목록.py
@@ -0,0 +1,7 @@
+def solution(todo_list, finished):
+    result = []
+    for i in range(len(todo_list)):
+        if finished[i] == False:
+            result.append(todo_list[i])
+    return result
+        
