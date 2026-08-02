8db187a0852c409f5fb6f23b75db96242f90ce6c
Yeonjae Choi <yeonjaechoi25@gmail.com>
2026-08-02 17:55:10 +0900

    [level 0] Title: 특별한 이차원 배열 2, Time: 0.12 ms, Memory: 11.7 MB -BaekjoonHub

---
+++ 프로그래머스/0/181831. 특별한 이차원 배열 2/특별한 이차원 배열 2.py
@@ -0,0 +1,7 @@
+def solution(arr):
+    for i in range(len(arr) // 2 + 1):
+        for j in range(len(arr) // 2 + 1):
+            if arr[i][j] != arr[j][i]:
+                return 0
+    else:
+        return 1        
