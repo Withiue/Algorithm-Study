7a8f8ee0ddd47ce62efcfe2e78fb334e12b040de
Yeonjae Choi <yeonjaechoi25@gmail.com>
2026-09-19 22:51:54 +0900

    [level 1] Title: 두 개 뽑아서 더하기, Time: 0.38 ms, Memory: 11.5 MB -BaekjoonHub

--- 프로그래머스/1/68644. 두 개 뽑아서 더하기/두 개 뽑아서 더하기.py
+++ 프로그래머스/1/68644. 두 개 뽑아서 더하기/두 개 뽑아서 더하기.py
@@ -1,13 +1,12 @@
-# 1. 2개 뽑는 모든 조합으로 더하기
-# 2. 더한 값이 기존 answer 리스트에 없으면 추가하기
-# 3. answer 오름차순 정렬해서 return하기
+from itertools import combinations
 
 def solution(numbers):
-    answer = []
-    for i in range(len(numbers)):
-        for j in range(i+1, len(numbers)):
-            tmp_num = numbers[i] + numbers[j]
-            if tmp_num not in answer:
-                answer.append(tmp_num)
+    answer = set()
+    
+    for a, b in list(combinations(numbers, 2)):
+        answer.add(a + b)
+    
+    answer = list(answer)
     answer.sort()
+    
     return answer
